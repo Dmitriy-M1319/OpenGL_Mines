281c6d00fd1df56da0655b3cf8143d1911138313
dimonchik <dimon.5642014@yandex.ru>
2022-01-31 12:37:08 +0300

    Add drawning.c

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ OPENGLFLAGS = -lglut -lGL -lGLU
 
 all: mines
 
-mines: main.c mine.o
-	$(CC) $(CFLAGS) main.c mine.o $(OPENGLFLAGS) -o mines
+mines: main.c mine.o drawning.o
+	$(CC) $(CFLAGS) main.c mine.o drawning.o $(OPENGLFLAGS) -o mines
 
 mine.o: ./lib/mine.c
 	$(CC) $(CFLAGS) -c ./lib/mine.c
+
+drawning.o: ./lib/drawning.c
+	$(CC) $(CFLAGS) -c ./lib/drawning.c -lGL
 
 clean: mine.o
 	rm mine.o mines
---
+++ lib/drawning.c
@@ -0,0 +1,61 @@
+#include <GL/gl.h>
+#include "drawning.h"
+
+void DrawEmptyCell()
+{
+    glBegin(GL_POLYGON);
+        glColor3f(0, 0.7f, 0); glVertex2f(0.0, 0.0);
+        glColor3f(0, 0.8f, 0); glVertex2f(0.0, 1.0);
+        glColor3f(0, 0.7f, 0); glVertex2f(1.0, 1.0);
+        glColor3f(0, 0.6f, 0); glVertex2f(1.0, 0.0);
+    glEnd();
+}
+void DrawEmptyOpenedCell()
+{
+    glBegin(GL_POLYGON);
+        glColor3f(0.7, 0.7, 0.7); glVertex2f(0, 0);
+        glColor3f(0.8, 0.8, 0.8); glVertex2f(0, 1);
+        glColor3f(0.7, 0.7, 0.7); glVertex2f(1, 1);
+        glColor3f(0.6, 0.6, 0.6); glVertex2f(1, 0);
+    glEnd();
+}
+
+void DrawOpenedMine()
+{
+    glBegin(GL_POLYGON);
+        glColor3f(0.7, 0, 0); glVertex2f(0, 0);
+        glColor3f(0.8, 0, 0); glVertex2f(0, 1);
+        glColor3f(0.7, 0, 0); glVertex2f(1, 1);
+        glColor3f(0.6, 0, 0); glVertex2f(1, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glColor3f(0, 0, 0); glVertex2f(0.2, 0.2);
+        glColor3f(0, 0, 0); glVertex2f(0.2, 0.8);
+        glColor3f(0, 0, 0); glVertex2f(0.8, 0.8);
+        glColor3f(0, 0, 0); glVertex2f(0.8, 0.2);
+    glEnd();
+}
+
+void DrawFlag()
+{
+    glBegin(GL_POLYGON);
+        glColor3f(0.7, 0.7, 0.7); glVertex2f(0, 0);
+        glColor3f(0.8, 0.8, 0.8); glVertex2f(0, 1.0);
+        glColor3f(0.7, 0.7, 0.7); glVertex2f(1.0, 1.0);
+        glColor3f(0.6, 0.6, 0.6); glVertex2f(1.0, 0);
+    glEnd();
+
+    glColor3f(0, 1, 1);
+    glBegin(GL_TRIANGLES);
+        glVertex2f(0.2, 0.6);
+        glVertex2f(0.6, 0.8);
+        glVertex2f(0.6, 0.6);
+    glEnd();
+
+    glColor3f(0, 0, 0);
+    glBegin(GL_LINES);
+        glVertex2f(0.6, 0.8);
+        glVertex2f(0.6, 0);
+    glEnd();
+}
